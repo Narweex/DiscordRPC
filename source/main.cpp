6a9601d6b2a2e078f791a3aff69dcdf96de27384
Narweex <68287337+Narweex@users.noreply.github.com>
2024-02-11 16:16:33 +0100

    Opravy, editace config.json

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include "include/discord_register.h"
 #include "include/discord_rpc.h"
 #include <thread>
@@ -11,82 +11,70 @@
 
 constexpr auto config_file = "config.json";
 
-class DiscordRPC {
 
-public:
-    DiscordRPC(const char* bot_id) {
-        DiscordEventHandlers handlers;  //initialize handlers
-        memset(&handlers, 0, sizeof(handlers));
-
-        Discord_Initialize(bot_id, &handlers, true, nullptr);   //initialize with bot id
-        std::cout << "Initialized\n";
-    }
-
-    ~DiscordRPC() {
-        Discord_Shutdown();
-        std::cout << "Uninitialized\n";
-    }
-
-private:
- 
-public:
-    DiscordRichPresence discordPresence{}; //class to update the info
-  
-    void UpdatePresence(std::string discord_status) {
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.state = discord_status.c_str();
-        discordPresence.details = "Start modding today";
-        discordPresence.startTimestamp = 0;
-        discordPresence.largeImageKey = "a";
-        discordPresence.largeImageText = "paramods.xyz";
-        discordPresence.smallImageText = "paramods.xyz";
-        discordPresence.partyId = "https://paramods.xyz=";
-        discordPresence.joinSecret = "https://paramods.xyz";
-        Discord_UpdatePresence(&discordPresence);
-    }
-};
+#include "main.hpp"
 
 
 int main() {
     
     bool running = true;    //runnin?
        
-    if (!std::filesystem::exists(config_file)) {
-            std::ofstream configfile(config_file); //create config if not exists
+    if (!std::filesystem::exists(config_file)) {        //If config file doesnt exist ask for application id and open developer portal
+            std::ofstream configfile(config_file); 
 
 
-            std::cout << "Put in APP ID\n";
+            std::cout << "Put in APP ID: \n";
             std::string app_id;
             std::cin >> app_id;
 
             nlohmann::json config;
             config["app_id"] = app_id;
-            config["discord_status"] = "RPC by Narweex";
+            config["text1"] = "RPC by Narweex";
+            config["text2"] = "RPC by Narweex";
             configfile << config;
             
             ShellExecute(0, 0, "https://discord.com/developers", 0, 0, SW_SHOW);
             
-    }
+    } 
     
+    //open config and fetch info 
     std::ifstream config_load("config.json");
     nlohmann::json load;
     config_load >> load;
 
+
+    //if there is no info ask to fill config
     if (load.empty())
         std::cout << "Please fill in app ID in config.json";
 
-    std::string application_id = load["app_id"];
-    std::string discord_status = load["discord_status"];
 
-    DiscordRPC discord(application_id.c_str()); //initialize discord class
+    //Load information to variables
+    std::string application_id = load["app_id"];
+    std::string status_text1 = load["text1"];
+    std::string status_text2 = load["text2"];
+
+
+    DiscordRPC discord(application_id.c_str()); //initialize discord class with application id
     
         
+    std::cout << " Press ESC to EXIT \n Press F1 to start running on background. ";
+    //main loop
         while (running) {
 
-            if (GetKeyState(VK_ESCAPE) & 0x8000)    /*If ESC is pressed exit*/
+            if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)    /*If ESC is pressed exit*/
                 running = false;
 
-            discord.UpdatePresence(discord_status);/*Update presence*/
+            if (GetAsyncKeyState(VK_F1) & 0x8000) {
+                for (int i = 10; i > 0; i--) {
+                    std::cout << "\r Console will detach in " << i << " seconds... you can close this program by restarting your PC or in task manager";
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                }
+                FreeConsole();
+            }
+               
+
+            discord.UpdatePresence(status_text1.c_str(), status_text2.c_str());//Update presence
+           
             std::this_thread::sleep_for(std::chrono::milliseconds(100));     //wait for some optimization
         }
     
---
+++ source/main.hpp
@@ -0,0 +1,37 @@
+﻿class DiscordRPC {
+
+public:
+    DiscordRPC(const char* bot_id) {
+        DiscordEventHandlers handlers;  //initialize handlers
+        memset(&handlers, 0, sizeof(handlers));
+
+        Discord_Initialize(bot_id, &handlers, true, nullptr);   //initialize with bot id
+        std::cout << "Initialized\n";
+    }
+
+    ~DiscordRPC() {
+        Discord_Shutdown();
+        std::cout << "Uninitialized\n";
+    }
+
+private:
+
+public:
+    DiscordRichPresence discordPresence{}; //class to update the info
+
+    void UpdatePresence(const char* text1, const char* text2) {
+
+        memset(&discordPresence, 0, sizeof(discordPresence));
+        discordPresence.state = text1;
+        discordPresence.details = text2;
+        /* discordPresence.state = "✅PayPal Payment Supported";
+        discordPresence.details = "✅Official Reseller";*/
+        discordPresence.startTimestamp = 1507665886;
+        //discordPresence.endTimestamp = 1507665886; DISABLED BECAUSE OF TIME
+        discordPresence.largeImageKey = "thumbnail";
+        discordPresence.largeImageText = "paramods.xyz";
+        discordPresence.smallImageText = "Paramods.xyz";
+        discordPresence.partyId = "paramods.xyz";
+        Discord_UpdatePresence(&discordPresence);
+    }
+};
